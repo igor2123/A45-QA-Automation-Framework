a0a2bd5aa4e9f3a697e66c799a5084e02b516d84
igor2123 <igor.smirnov@testpro.io>
2023-04-21 05:46:11 -0500

    Update BaseTest.java

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-
 import java.time.Duration;
 
 
