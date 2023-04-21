e1af072480a1653767d10c28947038e78f213f83
igor2123 <igor.smirnov@testpro.io>
2023-04-20 20:21:30 -0500

    Created Homework17 edited code for BaseTest and LoginTest

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -9,7 +9,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
-import java.time.Duration;
 
 
 public class BaseTest {
