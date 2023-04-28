be65c3c60464db16efb2bbed5c26081a2a67c957
igor2123 <igor.smirnov@testpro.io>
2023-04-28 12:15:32 -0500

    Homework21.java

--- src/test/java/TestHomework.java
+++ src/test/java/TestHomework.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -11,22 +12,22 @@ public class TestHomework extends BaseTest{
         provideEmail("demo@class.com");
         providePassword("te$t$tudent");
         clickSubmit();
-        Thread.sleep(2000);
+
 
         WebElement search = driver.findElement(By.cssSelector("[type='search']"));
         search.sendKeys("Pluto");
 
-        WebElement viewAll = driver.findElement(By.cssSelector("div.results h1 > button"));
+        WebElement viewAll = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.results h1 > button")));
         viewAll.click();
-        Thread.sleep(2000);
 
-        WebElement firstSong = driver.findElement(By.cssSelector("section#songResultsWrapper td.title"));
+
+        WebElement firstSong = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("section#songResultsWrapper td.title")));
         firstSong.click();
-        Thread.sleep(2000);
 
-        WebElement addToButton = driver.findElement(By.cssSelector("section#songResultsWrapper button.btn-add-to"));
+
+        WebElement addToButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("section#songResultsWrapper button.btn-add-to")));
         addToButton.click();
-        Thread.sleep(1000);
+
 
         // search the playlist and click
         WebElement playlist = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//li[contains(text(),'kz-new playlist')]"));
