d80132a8ff6a76e3dc901512adcf8ac4542110c3
igor2123 <igor.smirnov@testpro.io>
2023-04-29 18:14:25 -0500

    Homework21.java

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -3,7 +3,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import java.time.Duration;
 
 public class LoginTests extends BaseTest {
