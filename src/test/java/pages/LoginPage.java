4d0d7ddc7085c5be6100665cb28a6e2aca606156
igor2123 <igor.smirnov@testpro.io>
2023-05-18 11:45:56 -0500

    Homework29

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+
 public class LoginPage extends BasePage {
     @FindBy(css = "button[type='submit']")
     protected WebElement submitButtonLocator;
